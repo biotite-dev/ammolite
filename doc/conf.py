1b84c92b3110039ea3968a2ff8de3c0589cbc963
Patrick Kunzmann <patrick.kunzm@gmail.com>
2020-04-09 16:35:08 +0200

    Work on documentation

---
+++ doc/conf.py
@@ -0,0 +1,73 @@
+# This source code is part of the Biotite package and is distributed
+# under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
+# information.
+
+__author__ = "Patrick Kunzmann"
+
+from os.path import realpath, dirname, join, basename
+import sys
+
+
+# Include 'src/' in PYTHONPATH
+# in order to import the 'biotite' package
+doc_path = dirname(realpath(__file__))
+package_path = join(dirname(doc_path), "src")
+sys.path.insert(0, package_path)
+import biotite2pymol
+
+
+#### General ####
+
+extensions = ["sphinx.ext.autodoc",
+              "sphinx.ext.autosummary",
+              "sphinx.ext.doctest",
+              "sphinx.ext.mathjax",
+              "sphinx.ext.viewcode",
+              "numpydoc"]
+
+templates_path = ["templates"]
+source_suffix = [".rst"]
+master_doc = "index"
+
+project = "biotite2pymol"
+copyright = "2017-2020, the Biotite contributors"
+version = biotite2pymol.__version__
+
+exclude_patterns = ["build"]
+
+pygments_style = "sphinx"
+
+todo_include_todos = False
+
+# Prevents numpydoc from creating an autosummary which does not work
+# properly due to Biotite's import system
+numpydoc_show_class_members = False
+
+autodoc_member_order = "bysource"
+
+
+#### HTML ####
+
+html_theme = "alabaster"
+html_static_path = ["static"]
+html_css_files = [
+    "biotite.css",
+    "https://fonts.googleapis.com/css?" \
+        "family=Crete+Round|Fira+Sans|&display=swap",
+]
+htmlhelp_basename = "Biotite2PymolDoc"
+html_sidebars = {"**": ["about.html",
+                        "navigation.html",
+                        "searchbox.html",]}
+html_theme_options = {
+    "description"   : "From Biotite to PyMOL - and back again",
+    "github_user"   : "biotite-dev",
+    "github_repo"   : "biotite2pymol",
+    "github_banner" : "true",
+    "github_button" : "true",
+    "github_type"   : "star",
+    "page_width"    : "1200px",
+    "fixed_sidebar" : "true",
+    
+    "sidebar_link_underscore" : "#FFFFFF"
+}
---
+++ doc/index.rst
@@ -0,0 +1,21 @@
+#############
+biotite2pymol
+#############
+
+This package enables the usage of objects from *Biotite* directly in PyMOL,
+when using its Python API:
+
+- Import :class:`AtomArray` and :class:`AtomArrayStack` objects into PyMOL -
+  without intermediate structure files.
+- Convert PyMOL objects into :class:`AtomArray` and :class:`AtomArrayStack`
+  objects
+- Use *Biotite*'s boolean masks for atom selection as PyMOL selection
+
+.. image:: demo/demo.gif
+
+.. toctree::
+   :maxdepth: 1
+   :hidden:
+   
+   introduction
+
