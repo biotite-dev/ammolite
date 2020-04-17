bc5967eccbe0828163e17673bbbecc6ff824a65a
Patrick Kunzmann <patrick.kunzm@gmail.com>
2020-04-17 11:55:58 +0200

    Finished 'show()' function

--- src/biotite2pymol/display.py
+++ src/biotite2pymol/display.py
@@ -1,10 +1,15 @@
 import tempfile
-from os.path import join
+import time
+import datetime
+from os.path import join, getsize
 import pymol
 from pymol import cmd as default_cmd
 
 
-def show(size=None, use_ray=False, pymol_instance=None):
+INTERVAL = 0.1
+
+
+def show(size=None, use_ray=False, pymol_instance=None, timeout=60):
     try:
         from IPython.display import Image
     except ImportError:
@@ -26,21 +31,30 @@ def show(size=None, use_ray=False, pymol_instance=None):
     else:
         ray = 0
     
-    ###
-    cmd.png("temp.png", width, height, ray=ray)
-    import time
-    time.sleep(1)
-    
-    return Image("temp.png")
-    ###
-    
     image_file = tempfile.NamedTemporaryFile(
         delete=False, prefix="biotite2pymol", suffix=".png"
     )
     image_file.close()
-    cmd.png(image_file.name, width, height, ray=ray)
-    import time
-    time.sleep(5)
-    image = Image(image_file.name)
     
-    return image
+    start_time = datetime.datetime.now()
+
+    cmd.png(image_file.name, width, height, ray=ray)
+    
+    while True:
+        # After 'timeout' seconds the loop exits with an error
+        if (datetime.datetime.now() - start_time).total_seconds() > timeout:
+            raise TimeoutError(
+                "No PNG image was output within the expected time"
+            )
+        
+        # Check if PyMOL has already written image data to file
+        if getsize(image_file.name) > 0:
+            break
+
+        time.sleep(INTERVAL)
+    
+    return Image(image_file.name)
+
+
+def TimeoutError(Exception):
+    pass
