953470c67139fd2ad19331cc571d1df886b33819
Patrick Kunzmann <patrick.kunzm@gmail.com>
2020-04-17 14:15:29 +0200

    Added 'setup_parameters()' function

--- src/biotite2pymol/startup.py
+++ src/biotite2pymol/startup.py
@@ -1,15 +1,27 @@
 import pymol
-from pymol import cmd
+from pymol import cmd as default_cmd
 
 
 def launch_pymol(*args):
-    pymol.finish_launching(["pymol"] + list(args))
-    cmd.reinitialize()
+    """
+    Launch *PyMOL* with the given command line arguments.
 
+    This is the recommended way to start *PyMOL* in GUI mode.
+    It starts *PyMOL* by calling :func:`pymol.finish_launching()`,
+    reinitializes *PyMOL* to clear the workspace and sets up necessary
+    parameters using :func:`setup_parameters()`.
+    """
+    pymol.finish_launching(["pymol"] + list(args))
+    default_cmd.reinitialize()
+    setup_parameters()
+
+
+def setup_parameters(pymol_instance=None):
+    if pymol_instance is None:
+        cmd = default_cmd
+    else:
+        cmd = pymol_instance.cmd
+    
     # The selections only work properly,
     # if the order stays the same after adding a model to PyMOL
     cmd.set("retain_order", 1)
-
-
-def quit_pymol():
-    cmd.quit()
